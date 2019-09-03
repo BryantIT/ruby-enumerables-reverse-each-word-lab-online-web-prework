819b30f87718a339ba8d91fc07b8b1a079cfd562
Bryant Richards <dragonseyeimages@gmail.com>
2019-09-03 23:25:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,13 @@ def reverse_each_word(string)
   end
   sentence1.join(" ")
 end
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  sentence2 = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse #reverse each word in the array
+  end
+  test_array.join(" ")
+end
+
